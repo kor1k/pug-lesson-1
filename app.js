4b0c1d7eab1f5e3e5ac603566e14259aa6ff58a4
Kostya <koortttt@gmail.com>
2018-11-24 16:11:10 +0200

    added files .pug and logic in app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-let express = require('expess');
+let express = require('express');
 const app = express();
 
-app.set('views','/views');
-app.set('view engine', 'pug');
+app.set('views','./views');
+app.set('view engine','pug');
 
-app.get("/", (req, res) => {
-
+app.get('/',(req,res)=>{
+    res.render('index', {title: 'Homepage', message:'Hello world!'})
 });
 
 app.listen(3000);
